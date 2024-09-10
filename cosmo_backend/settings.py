abdadc570176a32cf1e31b53a4ebaa2d0641613c
SMRFT <110091550+SMRFT@users.noreply.github.com>
2024-09-10 09:45:23 +0530

    Update settings.py

--- cosmo_backend/settings.py
+++ cosmo_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^++i9_0$54xip#wtj3c#4qt&p1q2)q=n8difm*gjwsl49dc1na
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['52.66.22.14']
 
 
 # Application definition
