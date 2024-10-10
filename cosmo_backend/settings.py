865a0cd3b644e4f50034f30bdaecc3243df8d9c8
SMRFT <110091550+SMRFT@users.noreply.github.com>
2024-10-10 11:50:58 +0530

    Update settings.py

--- cosmo_backend/settings.py
+++ cosmo_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^++i9_0$54xip#wtj3c#4qt&p1q2)q=n8difm*gjwsl49dc1na
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.66.22.14']
+ALLOWED_HOSTS = ['52.66.22.14','195.250.21.70', 'shinovadatabase.in']
 
 
 # Application definition
