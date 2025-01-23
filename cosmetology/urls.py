a41b9303b32ac2db1f83f0dab8c8a51831e105a0
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-01-23 15:55:21 +0530

    Update urls.py

--- cosmetology/urls.py
+++ cosmetology/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import registration,login,pharmacy_data,check_medicine_status,pharmacy_upload,delete_medicine,check_upcoming_visits,PatientDetailsView,save_billing_data,upload_file,get_file,update_stock,delete_billing_data,Complaints_list,Findings_list,Tests_list,Procedure_list,diagnosis_list
-from .views import get_pdf_file, upload_pdf,Patients_data,PatientView,Appointmentpost,AppointmentView,SummaryDetailCreate,vitalform,get_medicine_price,get_summary_by_interval,get_billing_by_interval,get_procedurebilling_by_interval,medical_history,update_billing_data,post_procedures_bill,get_procedures_bill
+from .views import registration,login,pharmacy_data,check_medicine_status,pharmacy_upload,delete_medicine,check_upcoming_visits,PatientDetailsView,save_billing_data,upload_file,get_file,update_stock,delete_billing_data,Complaints_list,Findings_list,Tests_list,Procedure_list,diagnosis_list,delete_procedure_data
+from .views import get_pdf_file, upload_pdf,Patients_data,PatientView,Appointmentpost,AppointmentView,SummaryDetailCreate,vitalform,get_medicine_price,get_summary_by_interval,get_billing_by_interval,get_procedurebilling_by_interval,medical_history,update_billing_data,post_procedures_bill,get_procedures_bill,summary_get
 
 urlpatterns = [
     path('registration/', registration, name='registration'),
@@ -17,6 +17,7 @@ urlpatterns = [
     path('AppointmentView/', AppointmentView, name='AppointmentView'),
     path('summary/post/', SummaryDetailCreate, name='summary-create'),
     path('summary/post/patient_details/', PatientDetailsView, name='patient-details'),
+    path('summary_get/', summary_get, name='summary_get'),
     path('medicine_name/data/', get_medicine_price, name='get_medicine_price'),
     path('summary/<str:interval>/', get_summary_by_interval, name='summary-by-interval'),
     path('billing/<str:interval>/', get_billing_by_interval, name='get_billing_by_interval'),
@@ -37,7 +38,10 @@ urlpatterns = [
     path('upload_file/', upload_file, name='upload_file'),
     path('get_file/', get_file, name='get_file'),
     path('upload_pdf/', upload_pdf, name='upload_pdf'),
+    path('delete_procedure_data/', delete_procedure_data, name='delete_procedure_data'),
     path('get_pdf_file/', get_pdf_file, name='get_pdf_file'),
+
+
 
 
 ]
